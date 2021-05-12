83a252556f9e028a8de164f9f72bcda9a5cdc350
Paul Martin <greatestloginnameever@gmail.com>
2021-05-12 16:39:48 +0100

    Fix setup_intent metadata in checkout create

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,16 +1,24 @@
 import time
 
+import stripe
+
 
 def test_checkout(checkout_session_data, selenium_go_to_checkout):
     time.sleep(60)
 
 
 def test_setup_checkout(checkout_session_data, selenium_go_to_setup_checkout):
+    session = checkout_session_data['session']
+    setup_intent = stripe.SetupIntent.retrieve(session['setup_intent'])
+    assert 'subscription_id' not in setup_intent['metadata'] == {}
+
+
+def test_setup_checkout_subscription(checkout_session_data, subscription,
+                                     selenium_go_to_setup_checkout_subscription):
+    session = checkout_session_data['session']
+    setup_intent = stripe.SetupIntent.retrieve(session['setup_intent'])
+    assert setup_intent['metadata']['subscription_id'] == subscription['id']
     time.sleep(60)
-
-
-def test_setup_checkout_subscription(checkout_session_data, selenium_go_to_setup_checkout_subscription):
-    print(checkout_session_data['session'])
 
 
 def test_billing_portal(billing_portal_data, selenium_go_to_billing_portal):
