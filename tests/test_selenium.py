1a281814d68ca17e3d9bf1c44d4dd48022340518
Paul Martin <greatestloginnameever@gmail.com>
2021-05-12 15:38:31 +0100

    Fix setup_intent metadata in checkout create

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,17 +1,17 @@
 import time
 
 
-def test_checkout(selenium_go_to_checkout):
+def test_checkout(checkout_session_data, selenium_go_to_checkout):
     time.sleep(60)
 
 
-def test_setup_checkout(selenium_go_to_setup_checkout):
+def test_setup_checkout(checkout_session_data, selenium_go_to_setup_checkout):
     time.sleep(60)
 
 
-def test_setup_checkout_subscription(selenium_go_to_setup_checkout_subscription):
-    time.sleep(60)
+def test_setup_checkout_subscription(checkout_session_data, selenium_go_to_setup_checkout_subscription):
+    print(checkout_session_data['session'])
 
 
-def test_billing_portal(selenium_go_to_billing_portal):
+def test_billing_portal(billing_portal_data, selenium_go_to_billing_portal):
     time.sleep(60)
